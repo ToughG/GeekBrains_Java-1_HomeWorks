73a9fd2128b7f2138b6d015006abcccc591917bc
Rashit <toughg0101@gmail.com>
2017-12-08 18:50:05 +0600

    Java1_Lesson5_homework - start work

---
+++ Lesson5_hw.java
@@ -0,0 +1,16 @@
+/**
+ * Java. Level 1. Lesson 5. Homework
+ *
+ * @author Rashit Ibragimov
+ * @version dated Dec 8, 2017
+ * @link https://github.com/ToughG/GeekBrains_Java-1_HomeWorks
+ */
+class Lesson5_hw{
+	
+public static void main(String[] args){
+
+}
+
+
+
+}
