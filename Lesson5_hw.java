95fc03c086a7acd93da8aab7c2a35e9c9a208311
Rashit <toughg0101@gmail.com>
2017-12-08 19:24:03 +0600

    add constructor with parameters to class Employee

--- Lesson5_hw.java
+++ Lesson5_hw.java
@@ -8,15 +8,26 @@
 class Lesson5_hw{
         
     public static void main(String[] args){
-
+        Employee emp1 = new Employee("Jack Willson", "Manager", "jacky80@gmail.com", "+996559814567", 50000, 37);
     }
 } 
 
 class Employee{
     String fio;
-    String position;
+    String pos;
     String email;
     String phnumb;
     int salary;
-    int age;  
+    int age; 
+    
+    public Employee(String fio,String pos,String email,String phnumb, int salary, int age){
+        this.fio = fio;
+        this.pos = pos;
+        this.email = email;
+        this.phnumb = phnumb;
+        this.salary = salary;
+        this.age = age;
+      
+    }
+   
 }      
