93fd4894cc53c804edc62318a42d85f2212c6f11
Rashit <toughg0101@gmail.com>
2017-12-08 19:54:08 +0600

    Java1_Lesson5_homework_task1 - finish work (added array of objects and with cycle sorting)

--- Lesson5_hw.java
+++ Lesson5_hw.java
@@ -8,11 +8,18 @@
 class Lesson5_hw{
         
     public static void main(String[] args){
-        Employee emp1 = new Employee("Jack Willson", "Manager", "jacky80@gmail.com", "+996559814567", 50000, 37);
-        emp1.printObject();
+        Employee[] empArray = new Employee[5];
+        empArray[0] = new Employee("Jack Willson", "Team Leader", "jacky80@gmail.com", "+996559814567", 80000, 42);
+        empArray[1] = new Employee("Peter Parker", "System Administrator", "pit-lord@gmail.com", "+996550645872", 40000, 45);
+        empArray[2] = new Employee("Bill Ghostman", "Back-end developer", "ghostemane@gmail.com", "+996777425678", 90000, 25);
+        empArray[3] = new Employee("Gustav Ahr", "Front-end developer", "peepe_goth@gmail.com", "+996559345678", 100000, 21);
+        empArray[4] = new Employee("Maria Hill", "Designer", "kittyflap@gmail.com", "+996708467867", 70000, 24);
+        for(int i = 0; i<5; i++){
+            if(empArray[i].age>40)
+                empArray[i].printObject();
+        }
     }
 } 
-
 class Employee{
     String fio;
     String pos;
